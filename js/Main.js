92abbc0ced4381a2bc89783ae3ebb7242b934f47
thesmertnik <me@kazuschik.ru>
2022-11-25 20:13:32 +0300

    smertnik

--- js/Main.js
+++ js/Main.js
@@ -4,17 +4,56 @@
   const navbarLinks = document.getElementsByClassName('navbar-links') [0]
   //При клике на кнопку меню переключаем в состояние меню
   toggleButton.addEventListener('click',() => navbarLinks.classList.toggle('active'))
-
 }()
 
-//закрытие мею при клике вне меню
-$(".navbar").click(function (){
-  $('.toggle-button').toggle();
-});
-$(document).on('click', function (e){
-  if(!$(e.target).closest(".home").length){
-    $('.navbar').hidden();
+//Modal window "about me"
+let modal = document.getElementById('myModal');
+let btn = document.getElementById('myBtn');
+let span = document.getElementsByClassName('close')[0];
+btn.onclick = function (){
+  modal.style.display = 'block';
+}
+span.onclick = function (){
+  modal.style.display = 'none';
+}
+window.onclick = function (event){
+  if(event.target === modal){
+    modal.style.display = 'none';
   }
-  e.stopPropagation();
-});
+}
+//slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change image every 2 seconds
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+}
+
+
+
+
 
